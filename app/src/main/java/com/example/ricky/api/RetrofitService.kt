aed08407c8bc67cbab29523adf4851a0cedf3806
kanake10 <10kanake@gmail.com>
2022-03-27 23:56:08 +0300

    commit

---
+++ app/src/main/java/com/example/ricky/api/RetrofitService.kt
@@ -0,0 +1,13 @@
+package com.example.ricky.api
+
+import com.example.ricky.core.Constants.END_POINT
+import com.example.ricky.data.models.RickyMortyCharacter
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface RetrofitService {
+    @GET(END_POINT)
+    suspend fun getCharacters(
+        @Query
+            ("page") query: Int) : RickyMortyCharacter
+}
---
+++ app/src/main/java/com/example/ricky/core/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.ricky.core
+
+object Constants {
+    const val BASE_URL : String = "https://rickandmortyapi.com/api/"
+    const val END_POINT : String = "character"
+    private const val FIRST_PAGE_INDEX = 1
+}
