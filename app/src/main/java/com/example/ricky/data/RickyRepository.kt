51850eb369362cce05f1554fd9bca50ce5b82113
kanake10 <10kanake@gmail.com>
2022-03-27 23:55:36 +0300

    repo

---
+++ app/src/main/java/com/example/ricky/data/RickyRepository.kt
@@ -0,0 +1,22 @@
+package com.example.ricky.data
+
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.example.ricky.api.RetrofitService
+import com.example.ricky.data.models.CharacterData
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class RickyRepository @Inject constructor
+    (private val retrofitService: RetrofitService) {
+
+    fun getCharacter() : Flow<PagingData<CharacterData>> {
+        return Pager(
+            config = PagingConfig(enablePlaceholders = false, pageSize = 30),
+            pagingSourceFactory = {
+                CharacterPagingSource(retrofitService)
+            }
+        ).flow
+    }
+}
