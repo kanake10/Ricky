6023ada1ec7f47a60565168d2d77ac71ca526ed0
kanake10 <10kanake@gmail.com>
2022-03-27 23:55:10 +0300

    viewmodel

---
+++ app/src/main/java/com/example/ricky/ui/viewmodel/RickyViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.ricky.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+import com.example.ricky.data.RickyRepository
+import com.example.ricky.data.models.CharacterData
+
+@HiltViewModel
+class RickyViewModel @Inject constructor
+    (private val rickyRepository: RickyRepository) : ViewModel() {
+
+    fun getListData() : Flow<PagingData<CharacterData>> {
+        return rickyRepository.getCharacter().cachedIn(viewModelScope)
+    }
+}
