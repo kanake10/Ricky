088b35ffa842c25b462a1d465a3727f0c3d36a81
kanake10 <10kanake@gmail.com>
2022-03-28 20:47:19 +0300

    commit

--- app/src/main/java/com/example/ricky/data/RickyRepository.kt
+++ app/src/main/java/com/example/ricky/data/RickyRepository.kt
@@ -4,6 +4,7 @@ import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import com.example.ricky.api.RetrofitService
+import com.example.ricky.data.datastore.CharacterPagingSource
 import com.example.ricky.data.models.CharacterData
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
