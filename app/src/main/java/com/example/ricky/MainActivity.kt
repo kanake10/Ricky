8a20e131ac074d1a6206bacf3bb3fe67d13235f1
kanake10 <10kanake@gmail.com>
2022-05-24 11:59:20 +0300

    hidden action bar

--- app/src/main/java/com/example/ricky/MainActivity.kt
+++ app/src/main/java/com/example/ricky/MainActivity.kt
@@ -21,6 +21,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+          actionBar?.hide()
+          supportActionBar?.hide()
     }
 }
